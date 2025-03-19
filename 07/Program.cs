8515ae78f72ca69ddc946d38ea36f1e10d9b79a1
TharsoUniaraxa <g82mzsuk@gmail.com>
2025-03-19 16:26:18 -0300

    Add files via upload

---
+++ 07/Program.cs
@@ -0,0 +1,48 @@
+﻿int n;
+int i;
+string[] sexoaltura;
+double altura;
+string sexo;
+double menorAltura = 3;
+double maiorAltura = 0;
+double mediaMulher = 0;
+double quntdHomem = 0;
+double quntdMulher = 0;
+double somaAlturaMulher = 0;
+
+Console.WriteLine("Digite quantas vezes será lido:");
+n = int.Parse(Console.ReadLine());
+
+for (i = 0; i < n; i++)
+{
+    Console.WriteLine("Digite a altura e o sexo (m ou f):");
+    sexoaltura = Console.ReadLine().Split();
+    altura = double.Parse(sexoaltura[0]);
+    sexo = (sexoaltura[1]);
+
+    if (altura <= menorAltura)
+    {
+        menorAltura = altura;
+    }
+    if (altura >= maiorAltura)
+    {
+        maiorAltura = altura;
+    }
+    if (sexo == "m")
+    {
+        quntdHomem++;
+    }
+    else
+    {
+        quntdMulher++;
+        somaAlturaMulher += altura;
+    }
+    if (sexo == "f")
+    {
+        mediaMulher = somaAlturaMulher / quntdMulher;
+    }
+}
+Console.WriteLine($"menor altura: {(menorAltura):F2}");
+Console.WriteLine($"maior altura: {(maiorAltura):F2}");
+Console.WriteLine($"media da mulher: {(mediaMulher):F2}");
+Console.WriteLine($"numero de homens: {quntdHomem}");
---
+++ 08/Program.cs
@@ -0,0 +1,52 @@
+﻿int n;
+int i;
+string[] compravenda;
+string produto;
+double compra;
+double venda;
+double lucro;
+double percentualLucro;
+double LucroAbaixo = 0;
+double LucroEntre = 0;
+double LucroAcima = 0;
+double ValorTotalCompra = 0;
+double ValorTotalVenda = 0;
+double LucroTotal = 0;
+
+Console.WriteLine("Digite quantas vezes será lido:");
+n = int.Parse(Console.ReadLine());
+
+for (i = 0; i < n; i++)
+{
+    Console.WriteLine("Digite o nome da compra, o valor de compra o valor de venda:");
+    compravenda = Console.ReadLine().Split();
+    produto = compravenda[0];
+    compra = double.Parse(compravenda[1]);
+    venda = double.Parse(compravenda[2]);
+
+    lucro = venda - compra;
+    percentualLucro = (lucro / compra) * 100;
+
+    ValorTotalCompra += compra;
+    ValorTotalVenda += venda;
+    LucroTotal += lucro;
+
+    if (percentualLucro < 10)
+    {
+        LucroAbaixo++;
+    }
+    else if (percentualLucro >= 10 && percentualLucro <= 20)
+    {
+        LucroEntre++;
+    }
+    else
+    {
+        LucroAcima++;
+    }
+}
+Console.WriteLine($"lucro abaixo de 10%: {LucroAbaixo}");
+Console.WriteLine($"lucro entre 10% e 20%: {LucroEntre}");
+Console.WriteLine($"lucro acima 20%: {LucroAcima}");
+Console.WriteLine($"valor total de compra: {(ValorTotalCompra):F2}");
+Console.WriteLine($"valor total de Venda: {(ValorTotalVenda):F2}");
+Console.WriteLine($"lucro total: {(LucroTotal):F2}");
